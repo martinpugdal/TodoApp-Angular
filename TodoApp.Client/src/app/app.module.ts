09a3c4c005b7df9fe0d30026b74b4ed8a52c6473
Martin <martinpugdal@live.dk>
2024-11-06 13:52:10 +0100

    added board-card
    got rid of the test component we made last commit too

--- TodoApp.Client/src/app/app.module.ts
+++ TodoApp.Client/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { TestComponent } from './test/test.component';
+import { BoardCardComponent } from './board-card/board-card.component';
 
 @NgModule({
   declarations: [
         AppComponent,
         BoardComponent,
-        TestComponent 
+        TestComponent,
+        BoardCardComponent 
   ],
   imports: [
       BrowserModule,
---
+++ TodoApp.Client/src/app/board-card/board-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-card',
+  templateUrl: './board-card.component.html',
+  styleUrl: './board-card.component.css'
+})
+export class BoardCardComponent {
+
+}
